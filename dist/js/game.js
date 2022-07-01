b374c896fe6c8b380903b0e7c8f9d40ee97916ed
Renan Sgubin <78612307+RenanSgubin@users.noreply.github.com>
2022-06-30 22:44:37 -0300

    -Adicionado os arquivos
    -Melhorada a movimentação da nave

--- dist/js/game.js
+++ dist/js/game.js
@@ -1,11 +1,61 @@
 "use strict";
-let moveDistance = 0;
-window.onload = () => {
-    let changeShipImg = document.getElementById("space-ship");
+let directionYPlayer;
+let directionXPlayer;
+let playerPositionX = 0;
+let playerPositionY = 0;
+let playerVelocity = 5;
+let gameStart;
+let frame = 0;
+const player = document.getElementById("space-ship");
+const screenWidth = window.innerWidth;
+const screenHeight = window.innerHeight;
+function keyDown() {
     document.addEventListener("keydown", (e) => {
         if (e.key === "w") {
-            moveDistance += 10;
-            changeShipImg.style.bottom = moveDistance + "px";
+            directionYPlayer = -1;
+        }
+        if (e.key === "s") {
+            directionYPlayer = 1;
+        }
+        if (e.key === "d") {
+            directionXPlayer = 1;
+        }
+        if (e.key === "a") {
+            directionXPlayer = -1;
         }
     });
-};
+}
+function keyUp() {
+    document.addEventListener("keyup", (e) => {
+        if (e.key === "w" || e.key === "s") {
+            directionYPlayer = 0;
+        }
+        if (e.key === "d" || e.key === "a") {
+            directionXPlayer = 0;
+        }
+    });
+}
+function playerControl() {
+    playerPositionY += directionYPlayer * playerVelocity;
+    playerPositionX += directionXPlayer * playerVelocity;
+    player.style.top = playerPositionY + "px";
+    player.style.left = playerPositionX + "px";
+}
+function gameLoop() {
+    if (gameStart) {
+        playerControl();
+    }
+    frame = requestAnimationFrame(gameLoop);
+}
+function startGame() {
+    gameStart = true;
+    playerPositionX = screenWidth / 2;
+    playerPositionY = screenHeight / 2;
+    directionXPlayer = directionYPlayer = 0;
+    player.style.top = playerPositionY + "px";
+    player.style.left = playerPositionX + "px";
+    gameLoop();
+}
+window.addEventListener("load", startGame);
+document.addEventListener("keydown", keyDown);
+document.addEventListener("keyup", keyUp);
--- src/game.ts
+++ src/game.ts
@@ -1,22 +1,88 @@
 
 
-let moveDistance: number = 0;
+let directionYPlayer: number;
+let directionXPlayer: number;
 
-window.onload = () => {
+let playerPositionX: number = 0;
+let playerPositionY: number = 0;
 
-  let changeShipImg = document.getElementById("space-ship") as HTMLImageElement;
+let playerVelocity: number = 5;
 
+let gameStart: boolean;
+let frame: number = 0;
+const player = document.getElementById("space-ship") as HTMLImageElement;
+
+const screenWidth: number = window.innerWidth;
+const screenHeight: number = window.innerHeight;
+
+
+function keyDown() {
   document.addEventListener("keydown", (e) => {
-  
     if(e.key === "w") {
-  
-      moveDistance+=10;
-      changeShipImg.style.bottom = moveDistance+"px";
+      directionYPlayer = -1;
     }
   
-  })
+    if(e.key === "s") {
+      directionYPlayer = 1;
+    }
   
+    if(e.key === "d") {
+      directionXPlayer = 1;
+    }
+  
+    if(e.key === "a") {
+      directionXPlayer = -1;
+    }
+  
+  });
 }
 
+function keyUp() {
 
+  document.addEventListener("keyup", (e) => {
+    if(e.key === "w" || e.key === "s") {
+      directionYPlayer = 0;
+    }
+    if(e.key === "d" || e.key === "a") {
+      directionXPlayer = 0;
+    }
+  
+  });
 
+}
+
+function playerControl() {
+  playerPositionY += directionYPlayer * playerVelocity;
+  playerPositionX += directionXPlayer * playerVelocity;
+
+  player.style.top = playerPositionY+"px";
+  player.style.left = playerPositionX+"px";
+}
+
+function gameLoop() {
+  if(gameStart) {
+    playerControl();
+  }
+
+  frame = requestAnimationFrame(gameLoop);
+}
+
+function startGame() {
+
+  gameStart = true;
+
+  playerPositionX = screenWidth / 2;
+  playerPositionY = screenHeight / 2;
+
+  directionXPlayer = directionYPlayer = 0;
+  
+  player.style.top = playerPositionY+"px";
+  player.style.left = playerPositionX+"px";
+
+  gameLoop();
+
+}
+
+window.addEventListener("load",startGame);
+document.addEventListener("keydown",keyDown);
+document.addEventListener("keyup",keyUp);

a880d80b788d12751cb3b27a04a8e2372993b4de
Renan Sgubin <78612307+RenanSgubin@users.noreply.github.com>
2022-07-02 22:29:55 -0300

    -Adicionado os arquivos
    - Adicionada algumas sprites de inimigos
    - Adicionado placar de score
    - Iniciado o código para atirar

--- dist/js/game.js
+++ dist/js/game.js
@@ -3,7 +3,13 @@ let directionYPlayer;
 let directionXPlayer;
 let playerPositionX = 0;
 let playerPositionY = 0;
+let toW;
+let toS;
+let toD;
+let toA;
+let toSpaceBar;
 let playerVelocity = 5;
+let keyPress;
 let gameStart;
 let frame = 0;
 const player = document.getElementById("space-ship");
@@ -11,18 +17,12 @@ const screenWidth = window.innerWidth;
 const screenHeight = window.innerHeight;
 function keyDown() {
     document.addEventListener("keydown", (e) => {
-        if (e.key === "w") {
-            directionYPlayer = -1;
-        }
-        if (e.key === "s") {
-            directionYPlayer = 1;
-        }
-        if (e.key === "d") {
-            directionXPlayer = 1;
-        }
-        if (e.key === "a") {
-            directionXPlayer = -1;
-        }
+        toW = e.key === "w" ? directionYPlayer = -1 : false;
+        toS = e.key === "s" ? directionYPlayer = 1 : false;
+        toD = e.key === "d" ? directionXPlayer = 1 : false;
+        toA = e.key === "a" ? directionXPlayer = -1 : false;
+        toSpaceBar = e.key === "b" ? shoot() : false;
+        keyPress = e.key;
     });
 }
 function keyUp() {
@@ -35,16 +35,36 @@ function keyUp() {
         }
     });
 }
+function shoot() {
+    const shootDiv = document.createElement('div');
+    shootDiv.classList.add('shoot');
+    document.body.appendChild(shootDiv);
+    shootDiv.style.top = player.getBoundingClientRect().top + "px";
+    shootDiv.style.left = player.getBoundingClientRect().left + "px";
+}
 function playerControl() {
     playerPositionY += directionYPlayer * playerVelocity;
     playerPositionX += directionXPlayer * playerVelocity;
     player.style.top = playerPositionY + "px";
     player.style.left = playerPositionX + "px";
+    if (player.getBoundingClientRect().top <= 20 && keyPress === "w") {
+        playerVelocity = 0;
+    }
+    else if (player.getBoundingClientRect().bottom > screenHeight - 30 && keyPress === "s") {
+        playerVelocity = 0;
+    }
+    else if (player.getBoundingClientRect().left <= 30 && keyPress === "a") {
+        playerVelocity = 0;
+    }
+    else if (player.getBoundingClientRect().right > screenWidth - 30 && keyPress === "d") {
+        playerVelocity = 0;
+    }
+    else {
+        playerVelocity = 5;
+    }
 }
 function gameLoop() {
-    if (gameStart) {
-        playerControl();
-    }
+    playerControl();
     frame = requestAnimationFrame(gameLoop);
 }
 function startGame() {
--- src/game.ts
+++ src/game.ts
@@ -6,7 +6,15 @@ let directionXPlayer: number;
 let playerPositionX: number = 0;
 let playerPositionY: number = 0;
 
+let toW: boolean | number;
+let toS: boolean | number;
+let toD: boolean | number;
+let toA: boolean | number;
+let toSpaceBar: boolean | void;
+
 let playerVelocity: number = 5;
+
+let keyPress: string;
 
 let gameStart: boolean;
 let frame: number = 0;
@@ -18,22 +26,12 @@ const screenHeight: number = window.innerHeight;
 
 function keyDown() {
   document.addEventListener("keydown", (e) => {
-    if(e.key === "w") {
-      directionYPlayer = -1;
-    }
-  
-    if(e.key === "s") {
-      directionYPlayer = 1;
-    }
-  
-    if(e.key === "d") {
-      directionXPlayer = 1;
-    }
-  
-    if(e.key === "a") {
-      directionXPlayer = -1;
-    }
-  
+    toW = e.key === "w" ? directionYPlayer = -1 : false;
+    toS = e.key === "s" ? directionYPlayer = 1 : false;
+    toD = e.key === "d" ? directionXPlayer = 1 : false;
+    toA = e.key === "a" ? directionXPlayer = -1 : false;
+    toSpaceBar = e.key === "b" ? shoot() : false;
+    keyPress = e.key;
   });
 }
 
@@ -51,19 +49,43 @@ function keyUp() {
 
 }
 
+function shoot() {
+  const shootDiv = document.createElement('div');
+  shootDiv.classList.add('shoot');
+  document.body.appendChild(shootDiv);
+
+  shootDiv.style.top = player.getBoundingClientRect().top + "px";
+  shootDiv.style.left = player.getBoundingClientRect().left + "px";
+}
+
 function playerControl() {
   playerPositionY += directionYPlayer * playerVelocity;
   playerPositionX += directionXPlayer * playerVelocity;
 
   player.style.top = playerPositionY+"px";
   player.style.left = playerPositionX+"px";
+
+  if(player.getBoundingClientRect().top <= 20 && keyPress === "w") {
+    playerVelocity = 0;
+  }
+  else if(player.getBoundingClientRect().bottom > screenHeight - 30 && keyPress === "s") {
+    playerVelocity = 0;
+  }
+  else if(player.getBoundingClientRect().left <= 30 && keyPress === "a") {
+    playerVelocity = 0;
+  }
+  else if(player.getBoundingClientRect().right > screenWidth - 30 && keyPress === "d") {
+    playerVelocity = 0;
+  }
+  else {
+    playerVelocity = 5;
+  }
+
+
 }
 
 function gameLoop() {
-  if(gameStart) {
-    playerControl();
-  }
-
+  playerControl();
   frame = requestAnimationFrame(gameLoop);
 }
 
